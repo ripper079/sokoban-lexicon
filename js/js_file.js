e1540b963536660ed49887ccccdd08df6d2974e0
Daniel <powerdeveloper@fastmail.se>
2022-08-10 15:38:33 +0200

    Refactor of move functions

--- js/js_file.js
+++ js/js_file.js
@@ -10,7 +10,9 @@ let totalWidthContainerBlock = xSizeBlock * tileMap01.width;
 let totalHeightContainerBlock = ySizeBlock * tileMap01.height;
 
 let playerXCord = 11;
-let playerYCord = 11;; 
+let playerYCord = 11;
+
+let countGoals=0;
 
 // Add event listener on keydown
 document.addEventListener('keyup', (event) => {
@@ -86,6 +88,7 @@ let containerForBlocks = document.getElementById("container");
             //Goal color
             else if (tileMap01.mapGrid[y][x][0] === "G"){
                 divElement.classList.add(Tiles.Goal);
+                countGoals++;
             }
             //Empty color
             else {
@@ -113,146 +116,152 @@ let containerForBlocks = document.getElementById("container");
 
   
     function movePlayerUp(){
-
         console.log("Player moved UP[in function]");
         
-        // Get old cordinates for PLAYER
-        let oldXCordPlayer = playerXCord;
-        let oldYCordPlayer = playerYCord;
-        
-        //Get new Cordinates for PLAYER
-        let newXCordPlayer = oldXCordPlayer;
-        let newYCordPlayer = oldYCordPlayer - 1;
+        //Get cordinates for player
+        let xCordPlayer = playerXCord;
+        let yCordPlayer = playerYCord;
 
-        //Get new  cordinates for moved CRATE
-        let newXCordCrate = oldXCordPlayer;
-        let newYCordCrate = oldYCordPlayer - 2;
+        //Get cord for div above player
+        let xCordAbovePlayer = xCordPlayer;
+        let yCordAbovePlayer = yCordPlayer - 1;
 
-        //Id string PLAYER
-        let idStringPlayerOld = "x" + oldXCordPlayer + "y" + oldYCordPlayer;
-        let idStringUpperElement = "x" + newXCordPlayer + "y" + newYCordPlayer;  
-        //Id string moved CRATE
-        let idStringCrate = "x" + newXCordCrate + "y" + newYCordCrate;      
+        //Get cord 2 div above player
+        let xCordTwoAbovePlayer = xCordPlayer;
+        let yCordTwoAbovePlayer = yCordPlayer - 2;
 
-        // console.log("player cordinates:" + idStringPlayerOld);
-        // console.log("Block element cordinates above player: " + idStringUpperElement)
+        //Id strings
+        let idPlayer = "x" + xCordPlayer + "y" + yCordPlayer;
+        let idAbovePlayer = "x" + xCordAbovePlayer + "y" + yCordAbovePlayer;
+        let idTwoAbovePlayer = "x" + xCordTwoAbovePlayer + "y" + yCordTwoAbovePlayer;
 
-        let playerElement = document.getElementById(idStringPlayerOld);
-        let blockOverPlayer = document.getElementById(idStringUpperElement);
-        let crateNewPos = document.getElementById(idStringCrate);
+        //Get all div elements
+        let divPlayer = document.getElementById(idPlayer);
+        let divAbovePlayer = document.getElementById(idAbovePlayer);
+        let divTwoAbovePlayer = document.getElementById(idTwoAbovePlayer);
 
-        //Move UP to tile if possible
-        if (blockOverPlayer.className == Tiles.Space){
-            console.log("Block over player is:" + Tiles.Space + ". Movement UP is possible");
-            document.getElementById(idStringPlayerOld).classList.replace(Entities.Character, Tiles.Space);
-            document.getElementById(idStringUpperElement).classList.replace(Tiles.Space, Entities.Character);
-            playerYCord -= 1;
+        //Store classnames
+        let classNamePlayer = divPlayer.className;
+        let classNameAbovePlayer = divAbovePlayer.className;
+        let classNameTwoAbovePlayer = divTwoAbovePlayer.className;
+
+        // console.log("Player className: " + classNamePlayer);
+        // console.log("AbovePlayer classname: " + classNameAbovePlayer);
+        // console.log("TwoAbovePlayer classname: " + classNameTwoAbovePlayer);
+
+        //If above player tile is empty move one step up
+        if (classNameAbovePlayer == Tiles.Space){
+            console.log("One step movement up is ok");
+            //Remove existing classnames
+            divPlayer.classList.remove(classNamePlayer);
+            divAbovePlayer.classList.remove(classNameAbovePlayer);
+            
+            //And swap them
+            divPlayer.classList.add(classNameAbovePlayer);
+            divAbovePlayer.classList.add(classNamePlayer);
+
+            playerYCord--;
         }
-        // Move player one step up and move crate one step up
-        else if (blockOverPlayer.className == Entities.Block && crateNewPos.className == Tiles.Space){
-            console.log("It is possible to move the crate to up one step");
 
-            document.getElementById(idStringCrate).classList.replace(Tiles.Space, Entities.Block);
-            document.getElementById(idStringUpperElement).classList.replace(Entities.Block, Entities.Character);
-            document.getElementById(idStringPlayerOld).classList.replace(Entities.Character, Tiles.Space);
-            playerYCord -= 1;
-
-        }
-
-        
     }
 
     function movePlayerDown(){
         console.log("Player moved DOWN[in functions]");
 
-         // Get old cordinates for PLAYER        
-         let oldXCordPlayer = playerXCord;
-         let oldYCordPlayer = playerYCord;
-         
-         //Get new Cordinates for PLAYER
-         let newXCordPlayer = oldXCordPlayer;
-         let newYCordPlayer = oldYCordPlayer + 1;
+        //Get cordinates for player
+        let xCordPlayer = playerXCord;
+        let yCordPlayer = playerYCord;
 
-         //Get new cordinates for moved CRATE
-        let newXCordCrate = oldXCordPlayer;
-        let newYCordCrate = oldYCordPlayer + 2;
+        //Get cord for div below player
+        let xCordBelowPlayer = xCordPlayer;
+        let yCordBelowPlayer = yCordPlayer + 1;
 
-         //Id string player
-        let idStringPlayerOld = "x" + oldXCordPlayer + "y" + oldYCordPlayer;
-        let idStringLowerElement = "x" + newXCordPlayer + "y" + newYCordPlayer; 
-        //Id string moved CRATE
-        let idStringCrate = "x" + newXCordCrate + "y" + newYCordCrate; 
+        //Get cord 2 div below player
+        let xCordTwoBelowPlayer = xCordPlayer;
+        let yCordTwoBelowPlayer = yCordPlayer + 2;
 
-        console.log("player cordinates:" + idStringPlayerOld);
-        console.log("Block element cordinates belove player: " + idStringLowerElement)
+        //Id strings
+        let idPlayer = "x" + xCordPlayer + "y" + yCordPlayer;                
+        let idBelowPlayer = "x" + xCordBelowPlayer + "y" + yCordBelowPlayer;
+        let idTwoBelowPlayer = "x" + xCordTwoBelowPlayer + "y" + yCordTwoBelowPlayer;
 
-        let playerElement = document.getElementById(idStringPlayerOld);
-        let blockUnderPlayer = document.getElementById(idStringLowerElement);
-        let crateNewPos = document.getElementById(idStringCrate);
+        //Get all div elements
+        let divPlayer = document.getElementById(idPlayer);
+        let divBelowPlayer = document.getElementById(idBelowPlayer) 
+        let divTwoBelowPlayer = document.getElementById(idTwoBelowPlayer);
 
-        //Move DOWN to tile if possible
-        if (blockUnderPlayer.className == Tiles.Space) {
-            console.log("Block under player is:" + Tiles.Space + ". Movement DOWN is possible");
-            document.getElementById(idStringPlayerOld).classList.replace(Entities.Character, Tiles.Space);
-            document.getElementById(idStringLowerElement).classList.replace(Tiles.Space, Entities.Character);
-            playerYCord += 1;
+        //Store classnames
+        let classNamePlayer = divPlayer.className;
+        let classNameBelowPlayer = divBelowPlayer.className;
+        let classNameTwoBelowPlayer = divTwoBelowPlayer.className;
+
+        console.log("Player className: " + classNamePlayer);
+        console.log("BelowPlayer className:" + classNameBelowPlayer);
+        console.log("TwoBelowPlayer className: " + classNameTwoBelowPlayer);
+
+        //If below tile is empty move down
+        if (classNameBelowPlayer == Tiles.Space){
+            console.log("One step movement down is ok");
+            //Remove current classes
+            divPlayer.classList.remove(classNamePlayer);
+            divBelowPlayer.classList.remove(classNameBelowPlayer);
+
+            //And swap them
+            divPlayer.classList.add(classNameBelowPlayer);
+            divBelowPlayer.classList.add(classNamePlayer);
+
+            playerYCord++;
+
         }
-        // Move player one step down and move above crate one step down
-        else if (blockUnderPlayer.className == Entities.Block && crateNewPos.className == Tiles.Space){
-            console.log("It is possible to move crate one step down");
 
-            document.getElementById(idStringCrate).classList.replace(Tiles.Space, Entities.Block);
-            document.getElementById(idStringLowerElement).classList.replace(Entities.Block, Entities.Character);
-            document.getElementById(idStringPlayerOld).classList.replace(Entities.Character, Tiles.Space);
-            playerYCord += 1;
-        }
     }
 
     function movePlayerLeft(){
         console.log("Player moved LEFT[in function]");
 
-        // Get old cordinates for PLAYER        
-        let oldXCordPlayer = playerXCord;
-        let oldYCordPlayer = playerYCord;
-        
-        //Get new Cordinates for PLAYER
-        let newXCordPlayer = oldXCordPlayer - 1;
-        let newYCordPlayer = oldYCordPlayer;
-        
-        //Get new cordinates for moved CRATE
-        let newXCordCrate = oldXCordPlayer - 2;
-        let newYCordCrate = oldYCordPlayer;
+        //Get cordinates for player
+        let xCordPlayer = playerXCord;
+        let yCordPlayer = playerYCord;
 
-         //Id string player
-         let idStringPlayerOld = "x" + oldXCordPlayer + "y" + oldYCordPlayer;
-         let idStringLeftElement = "x" + newXCordPlayer + "y" + newYCordPlayer;
-         //Id string moved crate
-         let idStringCrate = "x" + newXCordCrate + "y" + newYCordCrate;
-        
-         //References to elements
-         let playerElement = document.getElementById(idStringPlayerOld);
-         let blockLeftToPlayer = document.getElementById(idStringLeftElement);
-         let crateNewPos = document.getElementById(idStringCrate);
+        //Get cord for div left of player
+        let xCordLeftPlayer = xCordPlayer - 1;
+        let yCordLeftPlayer = yCordPlayer;
 
-        let oldClassName = playerElement.className;
-        let newClassName = blockLeftToPlayer.className;
+        //Get cord 2 div on left player
+        let xCordTwoLeftPlayer = xCordPlayer - 2;
+        let yCordTwoLeftPlayer = yCordPlayer;
 
+        //Id strings
+        let idPlayer = "x" + xCordPlayer + "y" + yCordPlayer;                
+        let idLeftPlayer = "x" + xCordLeftPlayer + "y" + yCordLeftPlayer;
+        let idTwoLeftPlayer = "x" + xCordTwoLeftPlayer + "y" + yCordTwoLeftPlayer;
 
+        //Get all div elements
+        let divPlayer = document.getElementById(idPlayer);
+        let divLeftPlayer = document.getElementById(idLeftPlayer) 
+        let divTwoLeftPlayer = document.getElementById(idTwoLeftPlayer);
 
-         //Move LEFT to tile if possible
-        if (blockLeftToPlayer.className == Tiles.Space) {
-            console.log("Block to LEFT of player is:" + Tiles.Space + ". Movement LEFT is possible");
-            document.getElementById(idStringPlayerOld).classList.replace(Entities.Character, Tiles.Space);
-            document.getElementById(idStringLeftElement).classList.replace(Tiles.Space, Entities.Character);
-            playerXCord -= 1;
-        }
-        //Move LEFT to push block to left
-        else if (blockLeftToPlayer.className == Entities.Block && crateNewPos.className == Tiles.Space){
-            console.log("It is possible to move the crate to left one step");
-            document.getElementById(idStringCrate).classList.replace(Tiles.Space, Entities.Block);
-            document.getElementById(idStringLeftElement).classList.replace(Entities.Block, Entities.Character);
-            document.getElementById(idStringPlayerOld).classList.replace(Entities.Character, Tiles.Space);
-            playerXCord -= 1;
+        //Store classnames
+        let classNamePlayer = divPlayer.className;
+        let classNameLeftPlayer = divLeftPlayer.className;
+        let classNameTwoLeftPlayer = divTwoLeftPlayer.className;
+
+        console.log("Player className: " + classNamePlayer);
+        console.log("LeftPlayer className:" + classNameLeftPlayer);
+        console.log("TwoLeftPlayer className: " + classNameTwoLeftPlayer);
+
+        //If left tile is empty move left
+        if (classNameLeftPlayer == Tiles.Space){
+            console.log("One step to left is ok");
+            //Remove current classes
+            divPlayer.classList.remove(classNamePlayer);
+            divLeftPlayer.classList.remove(classNameLeftPlayer);
+
+            //And swap them
+            divPlayer.classList.add(classNameLeftPlayer);
+            divLeftPlayer.classList.add(classNamePlayer);
+
+            playerXCord--;
         }
 
     }
@@ -261,56 +270,56 @@ let containerForBlocks = document.getElementById("container");
     function movePlayerRight(){
         console.log("Player moved RIGHT[In function]");
 
-        // Get old cordinates for PLAYER        
-        let oldXCordPlayer = playerXCord;
-        let oldYCordPlayer = playerYCord;
-        
-        //Get new Cordinates for PLAYER
-        let newXCordPlayer = oldXCordPlayer + 1;
-        let newYCordPlayer = oldYCordPlayer;
+        //Get cordinates for player
+        let xCordPlayer = playerXCord;
+        let yCordPlayer = playerYCord;
 
-        //Get new cordinates for moved CRATE
-        let newXCordCrate = oldXCordPlayer + 2;
-        let newYCordCrate = oldYCordPlayer;
+        //Get cord for div right of player
+        let xCordRightPlayer = xCordPlayer + 1;
+        let yCordRightPlayer = yCordPlayer;
 
-         //Id string player
-         let idStringPlayerOld = "x" + oldXCordPlayer + "y" + oldYCordPlayer;
-         let idStringRightElement = "x" + newXCordPlayer + "y" + newYCordPlayer;
-         //Id string moved crate
-         let idStringCrate = "x" + newXCordCrate + "y" + newYCordCrate;  
-         
-        //References to elements
-         let playerElement = document.getElementById(idStringPlayerOld);
-         let blockRightToPlayer = document.getElementById(idStringRightElement);
-         let crateNewPos = document.getElementById(idStringCrate);
+        //Get cord of 2 divs on right player
+        let xCordTwoRightPlayer = xCordPlayer + 2;
+        let yCordTwoRightPlayer = yCordPlayer;
 
-         //Move RIGHT to tile if possible
-        if (blockRightToPlayer.className == Tiles.Space) {
-            console.log("Block to RIGHT of player is:" + Tiles.Space + ". Movement RIGHT is possible");
-            document.getElementById(idStringPlayerOld).classList.replace(Entities.Character, Tiles.Space);
-            document.getElementById(idStringRightElement).classList.replace(Tiles.Space, Entities.Character);
-            playerXCord += 1;
-        }
-        //Move RIGHT to push block to RIGHT
-        if (blockRightToPlayer.className == Entities.Block && crateNewPos.className == Tiles.Space){
-            console.log("It is possible to the the crate to right one step")
-            document.getElementById(idStringCrate).classList.replace(Tiles.Space, Entities.Block);
-            document.getElementById(idStringRightElement).classList.replace(Entities.Block, Entities.Character);
-            document.getElementById(idStringPlayerOld).classList.replace(Entities.Character, Tiles.Space);
-            playerXCord += 1;
+        //Id strings
+        let idPlayer = "x" + xCordPlayer + "y" + yCordPlayer;                
+        let idRightPlayer = "x" + xCordRightPlayer + "y" + yCordRightPlayer;
+        let idTwoRightPlayer = "x" + xCordTwoRightPlayer + "y" + yCordTwoRightPlayer;
+
+        //Get all div elements
+        let divPlayer = document.getElementById(idPlayer);
+        let divRightPlayer = document.getElementById(idRightPlayer) 
+        let divTwoRightPlayer = document.getElementById(idTwoRightPlayer);
+
+        //Store classnames
+        let classNamePlayer = divPlayer.className;
+        let classNameRightPlayer = divRightPlayer.className;
+        let classNameTwoRightPlayer = divTwoRightPlayer.className;
+
+        console.log("Player className: " + classNamePlayer);
+        console.log("RightPlayer className:" + classNameRightPlayer);
+        console.log("TwoRightPlayer className: " + classNameTwoRightPlayer);
+
+        //If right tile is empty move right
+        if (classNameRightPlayer == Tiles.Space){
+            console.log("Move one step to right is ok");
+
+            //Remove current classes
+            divPlayer.classList.remove(classNamePlayer);
+            divRightPlayer.classList.remove(classNameRightPlayer);
+
+            //And swap them
+            divPlayer.classList.add(classNameRightPlayer);
+            divRightPlayer.classList.add(classNamePlayer);
+
+            playerXCord++;
         }
     }
 
 
       //Game start
       createInitialMap();
-
-    
-
-
-
-
-
 
 
 
